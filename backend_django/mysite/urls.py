4779185839080ff9ee234a8aad659ee20d292937
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2024-11-14 19:44:35 +0530

    Add signup functionality.

--- backend_django/mysite/urls.py
+++ backend_django/mysite/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name = "home"),
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
